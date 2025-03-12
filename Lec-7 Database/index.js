614c4f7b459ba1d24125f7438105f83a89af16d4
Girish <rw3.girish.gk@gmail.com>
2025-03-12 11:11:06 +0530

    Mongoose Insert

--- Lec-7 Database/index.js
+++ Lec-7 Database/index.js
@@ -1,13 +1,33 @@
 const express = require('express');
-const db = require('./config/db')
+const db = require('./config/db');
+const student = require('./models/students');
 
 const app = express();
 const port = 8000;
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded()); // MiddleWare
 
 app.get('/', (req, res) => {
     res.render('form');
+})
+
+app.post('/addStud', (req, res) => {
+    console.log(req.body);
+
+    const { name, age, course } = req.body;
+
+    student.create({
+        name: name,
+        age: age,
+        course: course,
+    }).then(() => {
+        console.log("Data inserted...");
+    }).catch((err) => {
+        console.log("Error ", err);
+    })
+
+    res.redirect('/');
 })
 
 app.listen(port, () => console.log('Server started...'));
