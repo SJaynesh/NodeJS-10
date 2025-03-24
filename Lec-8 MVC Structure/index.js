7c27e91eb3027b6071238b2928579a0485fe5967
Darshan <desaidarshan616@gmail.com>
2025-03-24 10:33:39 +0530

    MVC Insert

--- Lec-8 MVC Structure/index.js
+++ Lec-8 MVC Structure/index.js
@@ -6,7 +6,10 @@ const port = 8000;
 
 app.set('view engine', 'ejs');
 
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/', require('./routes'));
+
 
 app.listen(port, (err) => {
     if (err) {
--- Lec-8 MVC Structure/routes/employee.js
+++ Lec-8 MVC Structure/routes/employee.js
@@ -6,4 +6,12 @@ const empCTR = require('../controllers/empController');
 
 route.get('/', empCTR.empPage);
 
+route.post('/addEMP', empCTR.insertEMP);
+
+route.get('/delEMP', empCTR.deleteEMP);
+
+route.get('/upEMP/:id', empCTR.updateEMP);
+
+route.post('/editEMP/:id', empCTR.editEMP);
+
 module.exports = route;
