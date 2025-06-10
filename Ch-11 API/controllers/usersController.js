1028a0cac2af9423ab2b0002438e968a7c66aea0
Darshan <desaidarshan616@gmail.com>
2025-06-10 12:15:27 +0530

    API

--- Ch-11 API/controllers/usersController.js
+++ Ch-11 API/controllers/usersController.js
@@ -1,9 +1,23 @@
 const usersModel = require("../models/usersModel");
 
-const fetchUser = (req, res) => {
-  res.status(200).json({ msg: "GET Request is called..." });
+// Fetch All Users
+const fetchUser = async (req, res) => {
+  try {
+    const usersData = await usersModel.find({});
+
+    if (usersData) {
+      res
+        .status(200)
+        .json({ msg: "Users records found...", records: usersData });
+    } else {
+      res.status(200).json({ msg: "Users records not found..." });
+    }
+  } catch (e) {
+    res.status(400).json({ msg: "Something Wrong", err: e });
+  }
 };
 
+// Insert User
 const insertUser = async (req, res) => {
   console.log(req.body);
 
@@ -15,7 +29,45 @@ const insertUser = async (req, res) => {
         .status(201)
         .json({ insert: true, msg: "User Inserted Successfully...." });
     } else {
-      res.status(400).json({ insert: false, msg: "User Insertion Failed...." });
+      res.status(200).json({ insert: false, msg: "User Insertion Failed...." });
+    }
+  } catch (e) {
+    res.status(400).json({ msg: "Something Wrong", err: e });
+  }
+};
+
+// Update User
+
+const updateUser = async (req, res) => {
+  try {
+    const updateData = await usersModel.findByIdAndUpdate(
+      req.params.id,
+      req.body
+    );
+
+    if (updateData) {
+      res.status(200).json({ update: true, msg: "User Record updated...." });
+    } else {
+      res
+        .status(200)
+        .json({ update: false, msg: "User Record updation failed...." });
+    }
+  } catch (e) {
+    res.status(400).json({ msg: "Something Wrong", err: e });
+  }
+};
+
+// Delete User
+const deleteUser = async (req, res) => {
+  try {
+    const deletedData = await usersModel.findByIdAndDelete(req.params.id);
+
+    if (deletedData) {
+      res.status(200).json({ delete: true, msg: "User Record deleted..." });
+    } else {
+      res
+        .status(200)
+        .json({ delete: false, msg: "User Record deletion failed..." });
     }
   } catch (e) {
     res.status(400).json({ msg: "Something Wrong", err: e });
@@ -25,4 +77,6 @@ const insertUser = async (req, res) => {
 module.exports = {
   fetchUser,
   insertUser,
+  deleteUser,
+  updateUser,
 };
--- Ch-11 API/routes/index.js
+++ Ch-11 API/routes/index.js
@@ -4,9 +4,23 @@ const route = express.Router();
 
 console.log("Routing...");
 
-const { fetchUser, insertUser } = require("../controllers/usersController");
+const {
+  fetchUser,
+  insertUser,
+  deleteUser,
+  updateUser,
+} = require("../controllers/usersController");
 
+// Fetch All Users
 route.get("/users", fetchUser);
-route.post("/users", insertUser)
+
+// Add User
+route.post("/users", insertUser);
+
+// Update User
+route.patch("/users/:id", updateUser);
+
+// Delete User
+route.delete("/users/:id", deleteUser);
 
 module.exports = route;
